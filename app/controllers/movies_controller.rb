8e53c9386da6016cb84e9e83d1dcd9a057e972d7
Bryce Beveridge <brycebev@berkeley.edu>
2022-10-01 01:47:15 +0000

    test heroku#4

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,7 +9,7 @@ class MoviesController < ApplicationController
     def index
       @movies = Movie.all
       @all_ratings = Movie.all_ratings
-      @ratings_to_show = @all_rating
+      @ratings_to_show = Movie.all_ratings
     end
   
     def new
