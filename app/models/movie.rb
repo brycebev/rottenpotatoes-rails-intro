0b4f86a785092e8207019b18d37268b8b7b90f85
Bryce Beveridge <brycebev@berkeley.edu>
2022-10-01 17:47:59 +0000

    movie rating test #1

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,11 +1,11 @@
 class Movie < ActiveRecord::Base
   def self.with_ratings(ratings_list)
+    puts @rating
     Movie.where(ratings_list.include?(@rating))
   end
 
   def self.all_ratings
     nums = Array.[]('G','PG','PG-13','R')
-    puts nums
     nums
   end
 end
