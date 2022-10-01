83d0f0daa013c3d071960af4f879e20949b8fe27
Bryce Beveridge <brycebev@berkeley.edu>
2022-10-01 17:35:39 +0000

    movie filtering #2

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,10 +1,10 @@
 class Movie < ActiveRecord::Base
   def self.with_ratings(ratings_list)
-    Movie.where(ratings_list.inclue? rating)
+    Movie.where(ratings_list.inclue? self.rating)
   end
 
   def self.all_ratings
-    nums = Array.[](1, 2, 3, 4,5)
+    nums = Array.[]('G','PG','PG-13','R')
     puts nums
     nums
   end
